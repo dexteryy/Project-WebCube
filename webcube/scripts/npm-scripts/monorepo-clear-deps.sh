f14dfaaf658a64923750cb856761825dbce2327c
Dexter Yang <dexter.yy@gmail.com>
2017-09-26 15:30:53 +0800

    fix: minor fix

--- webcube/scripts/npm-scripts/monorepo-clear-deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-(
-  lerna clean --yes 2>/dev/null || true
-) && (
-  rm -rf ./node_modules 2>/dev/null || true
-)
--- webcube/scripts/npm-scripts/monorepo-clear-lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-(
-  lerna exec -- rm yarn.lock package-lock.json 2>/dev/null || true
-) && (
-  rm yarn.lock package-lock.json 2>/dev/null || true
-)
