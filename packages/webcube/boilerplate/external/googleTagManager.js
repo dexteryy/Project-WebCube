ec2f2ad76a6e1823af9a8ad6393d9838547787fd
Dexter Yang <dexter.yy@gmail.com>
2018-01-25 20:27:46 +0800

    fix(webcube): google tag manager

--- packages/webcube/boilerplate/external/googleTagManager.js
+++ packages/webcube/boilerplate/external/googleTagManager.js
@@ -2,18 +2,16 @@
 export default function googleTagManager({ googleTagManagerContainerId }) {
   return {
     load() {
-      /* eslint-disable no-multi-assign */
-      const dataLayer = (window.dataLayer = window.dataLayer || []);
-      const gtag = (window.gtag = function gtag(...args) {
-        dataLayer.push(args);
-      });
-      /* eslint-enable no-multi-assign */
-      gtag('js', new Date());
-      gtag('config', googleTagManagerContainerId);
+      window.dataLayer = window.dataLayer || [];
+      window.gtag = function gtag(...args) {
+        window.dataLayer.push(args);
+      };
+      window.gtag('js', new Date());
+      window.gtag('config', googleTagManagerContainerId);
       const point = document.getElementsByTagName('script')[0];
       const gtmScript = document.createElement('script');
       gtmScript.async = true;
-      gtmScript.src = `https://www.googletagmanager.com/gtag.js?id=${googleTagManagerContainerId}`;
+      gtmScript.src = `https://www.googletagmanager.com/gtag/js?id=${googleTagManagerContainerId}`;
       point.parentNode.insertBefore(gtmScript, point);
     },
   };
