cf761f95470efe5c55f2c10c721d7d7aab54f218
Dexter Yang <dexter.yy@gmail.com>
2018-04-01 23:29:11 +0800

    fix(redux-source): hotfix

--- packages/redux-source/lib/index.js
+++ packages/redux-source/lib/index.js
@@ -93,6 +93,7 @@ const combineMethods = {
         }
       } else if (prevResult[resultName] === result[resultName]) {
         delete newResult[resultName];
+        isResultChange = true;
       }
     }
     if (isResultChange) {
