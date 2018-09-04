8723a9def5c5123902e116080bec75bd3345acd6
Dexter Yang <dexter.yy@gmail.com>
2018-09-04 15:49:57 +0800

    feat(webcube): add tracking and updater code

--- packages/webcube/scripts/create-cube-lint.sh
+++ packages/webcube/scripts/create-cube-lint.sh
@@ -2,18 +2,18 @@
 set -e
 
 echo 'Lint js...'
-npx eslint --fix --color --ext .js,.jsx,.ts,.tsx,.mjs,.mjsx,.es6 .
+npx eslint --fix --color --ext .js,.jsx,.ts,.tsx,.mjs,.mjsx,.es6 $* .
 
 echo 'Lint styled-components...'
-npx stylelint "**/*.js"
-npx stylelint "**/*.jsx"
-npx stylelint "**/*.ts"
-npx stylelint "**/*.tsx"
-npx stylelint "**/*.mjs"
-npx stylelint "**/*.mjsx"
-npx stylelint "**/*.es6"
+npx stylelint "**/*.js" $*
+npx stylelint "**/*.jsx" $*
+npx stylelint "**/*.ts" $*
+npx stylelint "**/*.tsx" $*
+npx stylelint "**/*.mjs" $*
+npx stylelint "**/*.mjsx" $*
+npx stylelint "**/*.es6" $*
 
 echo 'Lint css, scss, less...'
-npx stylelint "**/*.css" --fix
-npx stylelint "**/*.scss" --fix
-npx stylelint "**/*.less" --fix
+npx stylelint "**/*.css" $* --fix
+npx stylelint "**/*.scss" $* --fix
+npx stylelint "**/*.less" $* --fix
--- packages/webcube/scripts/create-cube.js
+++ packages/webcube/scripts/create-cube.js
@@ -1,8 +1,23 @@
 #!/usr/bin/env node
 const path = require('path');
 const program = require('commander');
+const updateNotifier = require('update-notifier');
+const Insight = require('insight');
 
 const pkg = require(path.join(__dirname, '../package.json'));
+
+const insight = new Insight({
+  trackingCode: 'UA-404086-15',
+  pkg,
+});
+if (insight.optOut === undefined) {
+  insight.askPermission();
+}
+
+updateNotifier({ pkg }).notify({
+  isGlobal: false,
+  shouldNotifyInNpmScript: true,
+});
 
 program
   .version(pkg.version)
